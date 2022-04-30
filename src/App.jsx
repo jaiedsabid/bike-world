d55065f9eedd463c20465abe1ba758cdf1a8bfbc
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-05-01 01:14:55 +0600

    Created 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+import PageLayout from './components/PageLayout';
 import Home from './pages/Home';
+import NotFound404 from './pages/NotFound404';
 
 const App = () => {
     return (
         <div className="App">
-            <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route
+                    path="/"
+                    element={
+                        <PageLayout>
+                            <Home />
+                        </PageLayout>
+                    }
+                />
                 <Route path="/blog" element={<h1>Blog</h1>} />
+                <Route path="/404" element={<NotFound404 />} />
             </Routes>
-            <Footer />
         </div>
     );
 };
