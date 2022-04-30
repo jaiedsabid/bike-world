089520a6c624c55cd1426be4214752f59adce6db
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-05-01 01:14:24 +0600

    Created a basic page layout

---
+++ src/components/PageLayout.jsx
@@ -0,0 +1,14 @@
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+const PageLayout = ({ children }) => {
+    return (
+        <>
+            <Navbar />
+            {children}
+            <Footer />
+        </>
+    );
+};
+
+export default PageLayout;
