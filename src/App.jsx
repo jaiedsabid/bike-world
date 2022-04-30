a037dcc6454684e5c2041d01cbf207eb12730041
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-05-01 00:50:59 +0600

    Created Hero/Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 const App = () => {
     return (
         <div className="App">
             <Navbar />
             <Routes>
-                <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/" element={<Home />} />
                 <Route path="/blog" element={<h1>Blog</h1>} />
             </Routes>
         </div>
