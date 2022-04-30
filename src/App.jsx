b7dec25a0823e0fa46f024b124de8e5a77cd43ce
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-04-30 15:11:13 +0600

    Installed React-Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
     return (
         <div className="App">
-            <h1>Hello world!</h1>
+            <Routes>
+                <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/blog" element={<h1>Blog</h1>} />
+            </Routes>
         </div>
     );
 };
