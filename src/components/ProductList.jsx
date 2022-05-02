2d811120b50bd5a98649fd5e9aeee8fd76bb1542
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-05-03 00:30:20 +0600

    Added add new item button

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ArrowNarrowRightIcon } from '@heroicons/react/outline';
+import { PlusSmIcon } from '@heroicons/react/solid';
 import { classNames } from '../utils/helpers';
 import ProductCard from './ProductCard';
+import Button from './Button';
 import { PRODUCTS } from '../data/dummyData';
 
 const ProductList = ({
     title,
     limit,
     manageInventoryBtn,
+    addNewItemBtn,
     enableDeleteBtn = false,
     className,
     ...props
@@ -29,6 +32,14 @@ const ProductList = ({
                     <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl md:text-center font-bold text-gray-900">
                         {title}
                     </h2>
+                )}
+
+                {addNewItemBtn && (
+                    <div className="py-4 w-full flex justify-end">
+                        <Button variant="primary">
+                            Add New Item <PlusSmIcon className="w-6 h-6 ml-1" />
+                        </Button>
+                    </div>
                 )}
 
                 <div className="mt-8 grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,5 +1,4 @@
 import { withProtectedRoute } from '../utils/withHOCs';
-import { PRODUCTS as products } from '../data/dummyData';
 import ProductList from '../components/ProductList';
 
 const Inventory = () => {
@@ -8,7 +7,7 @@ const Inventory = () => {
             <h2 className="text-2xl font-bold text-center leading-7 text-gray-900 sm:text-3xl sm:truncate">
                 Manage Inventory
             </h2>
-            <ProductList enableDeleteBtn />
+            <ProductList enableDeleteBtn addNewItemBtn />
         </div>
     );
 };
