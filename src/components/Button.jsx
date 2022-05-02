8b70ee3023bfb398b8d5b33af427f6a8b0a205a2
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-05-02 18:05:08 +0600

    Updated button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
+import { classNames } from '../utils/helpers';
+
 const Button = ({ children, onClick, ...props }) => {
     return (
         <button
             {...props}
             type="button"
-            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none"
+            className={classNames(
+                'inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none',
+                props.disabled ? 'opacity-50 cursor-not-allowed' : ''
+            )}
             onClick={() => {
                 if (typeof onClick === 'function') {
                     onClick();
