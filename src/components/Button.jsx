a27e5c4b00f24b36a46f987643350f68e0b44484
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-05-03 00:29:35 +0600

    Added button varient

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,19 @@
 import { classNames } from '../utils/helpers';
 
-const Button = ({ children, onClick, ...props }) => {
+const Button = ({ children, variant = 'secondary', onClick, ...props }) => {
+    const varients = {
+        primary: 'text-white bg-blue-600 hover:bg-blue-700',
+        secondary: 'text-blue-700 bg-blue-100 hover:bg-blue-200',
+    };
+
     return (
         <button
             {...props}
             type="button"
             className={classNames(
                 'inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none',
-                props.disabled ? 'opacity-50 cursor-not-allowed' : ''
+                props.disabled ? 'opacity-50 cursor-not-allowed' : '',
+                varients[variant]
             )}
             onClick={() => {
                 if (typeof onClick === 'function') {
